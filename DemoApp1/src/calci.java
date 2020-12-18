a4576ba7ba1ee884531012b575d5cc747d398105
Namrata-Kumar <76149016+Namrata-Kumar@users.noreply.github.com>
2020-12-18 16:51:45 +0530

    Update calci.java

--- DemoApp1/src/calci.java
+++ DemoApp1/src/calci.java
@@ -1,4 +1,4 @@
-
+package com.demo;
 
 class calci {
 	public static int add(int num1, int num2) {
@@ -8,10 +8,13 @@ class calci {
 	public static int diff(int num1, int num2) {
 		return num1 - num2;
 	}
+	public static int div(int num1, int num2) {
+		return num1 / num2;
+	}
 
 	public static void main(String args[]) {
-		System.out.println("add "+add(10, 20));
-		System.out.println("sub "+diff(10, 20));
-
+		System.out.println("Add="+ add(10, 20));
+		System.out.println("Diff="+ diff(10, 20));
+		System.out.println("Division="+ div(10, 20));
 	}
 }
